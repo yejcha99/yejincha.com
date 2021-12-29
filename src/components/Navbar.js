10bb95e3892e62824c584525fe98e365b5d5bd5e
yejcha99 <yejcha99@gmail.com>
2021-12-28 19:05:48 -0500

    changed navbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-custom">
             <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/></a>
             <div className="container">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
